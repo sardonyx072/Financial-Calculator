96e3ce8bd7aa4f8eca51942183b8a06bfa2e2d3d
hoff3539 <hoff3539@OSS230-19.stthomas.edu>
2016-12-16 18:08:20 -0600

    done

--- src/com/fin4/MonthTest.java
+++ src/com/fin4/MonthTest.java
@@ -11,7 +11,14 @@ public class MonthTest {
 	@Test
 	public void testGetStartBalance() {
 		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		System.out.println(test.getStartBalanceFormatted());
 		assertEquals(test.getStartBalance().doubleValue(),0.01,0);
+	}
+
+	@Test
+	public void testGetStartBalanceFormatted() {
+		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		assertEquals(test.getStartBalanceFormatted(),"$0.01");
 	}
 
 	@Test
@@ -21,9 +28,21 @@ public class MonthTest {
 	}
 
 	@Test
+	public void testGetInterestFormatted() {
+		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		assertEquals(test.getInterestFormatted(),"$0.02");
+	}
+
+	@Test
 	public void testGetTotalInterest() {
 		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
 		assertEquals(test.getTotalInterest().doubleValue(),0.03,0);
+	}
+
+	@Test
+	public void testGetTotalInterestFormatted() {
+		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		assertEquals(test.getTotalInterestFormatted(),"$0.03");
 	}
 
 	@Test
@@ -33,9 +52,21 @@ public class MonthTest {
 	}
 
 	@Test
+	public void testGetPaymentFormatted() {
+		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		assertEquals(test.getPaymentFormatted(),"$0.04");
+	}
+
+	@Test
 	public void testGetEndBalance() {
 		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
 		assertEquals(test.getEndBalance().doubleValue(),0.05,0);
+	}
+
+	@Test
+	public void testGetEndBalanceFormatted() {
+		Month test = new Month(new BigDecimal(1),new BigDecimal(2),new BigDecimal(3),new BigDecimal(4),new BigDecimal(5));
+		assertEquals(test.getEndBalanceFormatted(),"$0.05");
 	}
 
 	@Test
